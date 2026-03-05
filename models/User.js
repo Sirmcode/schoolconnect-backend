b262f88f8f7b850a0af2dfda4aff91928cac0678
Sirmcode <speak2sam2024@gmail.com>
2026-03-05 22:55:00 +0100

    Add models/User.js

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, 'Please add an email'],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please add a valid email'],
+    },
+    passwordHash: {
+        type: String,
+        required: [true, 'Please add a password'],
+        minlength: 6,
+    },
+    role: {
+        type: String,
+        enum: ['teacher', 'school', 'admin'],
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('User', userSchema);
